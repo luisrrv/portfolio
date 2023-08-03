ffdd805e05f78dc53f4ade0756d71d013696bdf8
luisrrv <rodluisr@gmail.com>
2023-08-03 18:23:48 +0900

    about responsiveness update

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -42,10 +42,12 @@ export default function About ({isDark, hideApp}: AboutProps) {
       <div className="content" style={zoomStyle}>
         {/* <div className="img"></div> */}
         <div className='special'>
+          <p className='small hello'>HELLO, I'M</p>
           <p className='big'>LUIS</p>
-          <p className='small'>LUIS RODRIGUEZ</p>
           <p className='big'>RODRIGUEZ</p>
-          </div>
+          <p className='small name'>LUIS RODRIGUEZ</p>
+        </div>
+        <div className="divider"></div>
         <div className="text">
           <p>A passionate Software Developer based in Tokyo. <br></br>Experienced in working on both front-end and back-end development for web and native apps.</p>
         </div>
