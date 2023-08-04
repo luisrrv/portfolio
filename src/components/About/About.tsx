6480bf727eb3fc9eaed79aed173b2168993cfbf8
luisrrv <rodluisr@gmail.com>
2023-08-04 10:05:25 +0900

    scrolling about animation improvement

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import './About.scss';
 
-// import * as React from 'react';
 interface AboutProps {
   isDark: boolean;
   hideApp: boolean;
 }
 
-export default function About ({isDark, hideApp}: AboutProps) {
+export default function About({ isDark, hideApp }: AboutProps) {
   const compClassName = isDark ? 'dark' : 'light';
 
   const [scrollPosition, setScrollPosition] = useState(0);
 
   // Update scroll position on scroll
   const handleScroll = () => {
-    setScrollPosition(window.scrollY);
+    requestAnimationFrame(() => {
+      setScrollPosition(window.scrollY);
+    });
   };
 
   // Add event listener on mount and remove it on unmount
@@ -35,21 +36,24 @@ export default function About ({isDark, hideApp}: AboutProps) {
   const zoomStyle: React.CSSProperties = {
     transform: `scale(${calculateScale()})`,
   };
-  
 
   return (
     <div className={`about_container ${compClassName} ${hideApp && 'hidden'}`}>
       <div className="content" style={zoomStyle}>
         {/* <div className="img"></div> */}
-        <div className='special'>
-          <p className='small hello'>HELLO, I'M</p>
-          <p className='big'>LUIS</p>
-          <p className='big'>RODRIGUEZ</p>
-          <p className='small name'>LUIS RODRIGUEZ</p>
+        <div className="special">
+          <p className="small hello">HELLO, I'M</p>
+          <p className="big">LUIS</p>
+          <p className="big">RODRIGUEZ</p>
+          <p className="small name">LUIS RODRIGUEZ</p>
         </div>
         <div className="divider"></div>
         <div className="text">
-          <p>A passionate Software Developer based in Tokyo. <br></br>Experienced in working on both front-end and back-end development for web and native apps.</p>
+          <p>
+            A passionate Software Developer based in Tokyo. <br></br>Experienced
+            in working on both front-end and back-end development for web and
+            native apps.
+          </p>
         </div>
       </div>
     </div>
