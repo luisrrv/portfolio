8b9be0e7193799f0b9c6bab222b43e5aa81767e5
luisrrv <rodluisr@gmail.com>
2023-09-05 12:15:56 +0900

    dialog close fix

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -33,23 +33,28 @@ export default function Header({ isDark, hideDialog, handleContactMouseOverChang
   const changeMode = () => {
     handleDarkModeChange(!isDark);
   };
+  const handleBackgroundClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) {
+      hideDialog();
+    }
+  };
 
   return (
-    <div id="info_bg" className={`close ${compClassName}`} onClick={hideDialog}>  
+    <div id="info_bg" className={`close ${compClassName}`} onClick={handleBackgroundClick}>  
         <div id='info_container' className={`info_container ${(window.matchMedia("(any-hover: none)").matches) ? 'mobile' : ''}`}>
             <div className="toggles">
               {isDark ? (
-                <div className="toggle dm"><MdSunny size={20} onClick={changeMode} /></div> 
+                <div className="toggle dm"><MdSunny size={20} onClick={() => changeMode()} /></div> 
               ) : (
-                <div className="toggle dm"><MdOutlineDarkMode size={20} onClick={changeMode} /></div> 
+                <div className="toggle dm"><MdOutlineDarkMode size={20} onClick={() => changeMode()} /></div> 
               )}
-              <div className="close"><AiOutlineClose size={28} /></div> 
+              <div className="close"><AiOutlineClose size={28} onClick={hideDialog} /></div> 
             </div>
             <div className="content">
                 <div className="nav">
-                  <a href="#" className="about"><RiUser5Fill />About me</a>
-                  <a href="#skills" className="skills"><BiSolidBrain />My skills</a>
-                  <a href="#work" className="work"><BiSolidBriefcaseAlt />Some of my work</a>
+                  <a href="#" className="about" onClick={hideDialog}><RiUser5Fill />About me</a>
+                  <a href="#skills" className="skills" onClick={hideDialog}><BiSolidBrain />My skills</a>
+                  <a href="#work" className="work" onClick={hideDialog}><BiSolidBriefcaseAlt />Some of my work</a>
                   <p className="contact"><BsGithub />GitHub</p>
                   <p className="contact"><BsLinkedin />LinkedIn</p>
                   <p className="contact"><MdAlternateEmail />Send me an Email</p>
