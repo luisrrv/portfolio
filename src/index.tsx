ed54009005472057b2c8c0699ae9c34e461431c8
luisrrv <rodluisr@gmail.com>
2025-06-19 12:56:16 +0900

    reflect route setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { LanguageProvider } from './context/LanguageContext';
-
+import Reflect from './pages/Reflect';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={
+          <LanguageProvider>
+            <App />
+          </LanguageProvider>
+          } />
+        <Route path="/reflect" element={<Reflect />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/pages/Reflect.tsx
@@ -0,0 +1,13 @@
+// src/pages/Reflect.tsx
+import React from 'react';
+
+const Reflect = () => {
+    return (
+        <div>
+            {/* TODO: Three.js orb + camera feed goes here */}
+            🚧 Reflect page under construction 🚧
+        </div>
+    );
+};
+
+export default Reflect;
